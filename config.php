64bfe972a6e1a207e2a442ece6880689a0d044c2
root <root@HOSTNAME=db109.(none)>
2012-10-02 08:35:33 +0800

    backup config

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.ybotic.com/');
+define('HTTP_IMAGE', 'http://shop.ybotic.com/image/');
+define('HTTP_ADMIN', 'http://shop.ybotic.com/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.ybotic.com/');
+define('HTTPS_IMAGE', 'http://shop.ybotic.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/www/shopilex/catalog/');
+define('DIR_SYSTEM', '/opt/www/shopilex/system/');
+define('DIR_DATABASE', '/opt/www/shopilex/system/database/');
+define('DIR_LANGUAGE', '/opt/www/shopilex/catalog/language/');
+define('DIR_TEMPLATE', '/opt/www/shopilex/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/www/shopilex/system/config/');
+define('DIR_IMAGE', '/opt/www/shopilex/image/');
+define('DIR_CACHE', '/opt/www/shopilex/system/cache/');
+define('DIR_DOWNLOAD', '/opt/www/shopilex/download/');
+define('DIR_LOGS', '/opt/www/shopilex/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'shopilex');
+define('DB_PASSWORD', 'shopilex');
+define('DB_DATABASE', 'shopilex');
+define('DB_PREFIX', '');
+?>
